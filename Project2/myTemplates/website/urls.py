8ab49e8f24acc8475c55413cc147faf01657f43e
nandinichhajed <nandinichhajed08@gmail.com>
2023-03-22 15:01:03 +0530

    about us page

--- Project2/myTemplates/website/urls.py
+++ Project2/myTemplates/website/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='HomePage'),
+    path('about', views.AboutPageView.as_view(), name='AboutPage'),
 ]
--- Project2/myTemplates/website/views.py
+++ Project2/myTemplates/website/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
