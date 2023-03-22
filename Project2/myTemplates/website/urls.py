a3a5d705e391a620fbe91b15be9710c384b44ef4
nandinichhajed <nandinichhajed08@gmail.com>
2023-03-22 15:04:14 +0530

    contact us page

--- Project2/myTemplates/website/urls.py
+++ Project2/myTemplates/website/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='HomePage'),
     path('about', views.AboutPageView.as_view(), name='AboutPage'),
+    path('contact', views.ContactPageView.as_view(), name='ContactPage'),
 ]
--- Project2/myTemplates/website/views.py
+++ Project2/myTemplates/website/views.py
@@ -8,3 +8,6 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
