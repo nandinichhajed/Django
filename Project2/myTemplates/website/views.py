ffd1f7d557ecb483a68762e8dfdf01e0698b9d1a
nandinichhajed <nandinichhajed08@gmail.com>
2023-03-22 14:52:59 +0530

    home page for proj 2

--- Project2/myTemplates/website/views.py
+++ Project2/myTemplates/website/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+# import requests
 # Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'index.html'
