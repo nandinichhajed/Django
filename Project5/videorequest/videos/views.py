6b9b595b5abd541e04d43a98c2144d44c4a9a121
nandinichhajed <nandinichhajed08@gmail.com>
2023-03-23 18:05:44 +0530

    setting up models

--- Project5/videorequest/videos/views.py
+++ Project5/videorequest/videos/views.py
@@ -3,7 +3,9 @@ from .models import *
 # Create your views here.
 
 def index(request):
-    return render(request, 'videorequest/index.html')
+    videos = Video.objects.order_by('-date_added')
+    context = {'videos': videos}
+    return render(request, 'videorequest/index.html', context)
 
 def vrform(request):
     return render(request, 'videorequest/vrform.html')
