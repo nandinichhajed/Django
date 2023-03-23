64fbe3d07f08e937b3a77dfeb88063fca542f0ef
nandinichhajed <nandinichhajed08@gmail.com>
2023-03-23 17:17:26 +0530

    making views

--- Project5/videorequest/videos/views.py
+++ Project5/videorequest/videos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def index(request):
+    return render(request, 'videorequest/index.html')
+
+def vrform(request):
+    return render(request, 'videorequest/vrform.html')
