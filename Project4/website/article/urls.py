a1ba86f3ee410a7beb7d7c9a3c7593421c0b208a
nandinichhajed <nandinichhajed08@gmail.com>
2023-03-23 16:12:41 +0530

    Setting up urls for article details page

--- Project4/website/article/urls.py
+++ Project4/website/article/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name='home'),
+    path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article_page'),
 ]
