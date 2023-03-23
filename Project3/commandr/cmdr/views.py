274f24ebebeb88085d49e9097544a4f5216833c9
nandinichhajed <nandinichhajed08@gmail.com>
2023-03-23 10:53:04 +0530

    project3 completed

--- Project3/commandr/cmdr/views.py
+++ Project3/commandr/cmdr/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import cmdr
 # Create your views here.
+
+class HomePageView(ListView):
+    model = cmdr
+    template_name = 'home.html'
