7b98ebb30bf3a1a5bb63509a3e77bae6f8f6d6d6
nandinichhajed <nandinichhajed08@gmail.com>
2023-03-30 17:23:31 +0530

    deleting proj6

--- Project6/website/article/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ArticleListView.as_view(), name='home'),
-    path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article_page'),
-    path('article/new', views.ArticleCreateView.as_view(), name='new_article'),
-]
