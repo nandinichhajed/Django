8d88a87f55857ef3d78eae2c7d6e9c86555143f6
nandinichhajed <nandinichhajed08@gmail.com>
2023-03-31 19:12:26 +0530

    configuring urls

--- Project7/website/website/settings.py
+++ Project7/website/website/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'todo',
 ]
 
 ROOT_URLCONF = 'website.urls'
