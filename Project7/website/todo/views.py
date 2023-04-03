cb169ff14dd358202145a82cc6dc7207a022ae38
nandinichhajed <nandinichhajed08@gmail.com>
2023-04-03 14:26:13 +0530

    delete and complete todo urls and views

--- Project7/website/todo/views.py
+++ Project7/website/todo/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+
+from .models import Todo
+from .forms import TodoForms
 # Create your views here.
 
 def index(request):
+    mytodo = Todo.objects.order_by('id')
+    form = TodoForms()
+    context = {'mytodo':mytodo, 'form':form}
     return render(request, "todo/index.html")
+
+def completeTodo(request, todo_id):
+    mytodo = Todo.objects.get(pk=todo_id)
+    mytodo.complete = True
+    mytodo.save()
+
+    return redirect('index')
+
+
+def deleteTodo(request):
+    Todo.objects.filter(complete__exact=True).delete()
+    return redirect('index')
