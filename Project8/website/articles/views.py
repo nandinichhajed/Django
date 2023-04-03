cdf83534d56ec142a71e2f13e95aec7c837b3823
nandinichhajed <nandinichhajed08@gmail.com>
2023-04-03 16:07:12 +0530

    adding proj 4 in proj 8

--- Project8/website/articles/views.py
+++ Project8/website/articles/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Article
+
 # Create your views here.
+
+from . models import Article
 
 class ArticleListView(ListView):
     model = Article
     template_name = 'home.html'
-    
+
+
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'detail.html'
-    context_object_name = 'articleDetail'
+    context_object_name = 'batman'
